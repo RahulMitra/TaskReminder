dd1fa843376b063fc065d1cfe62643ddfc376d8e
Rahul Mitra <rahulmitra.az@gmail.com>
2012-08-19 01:56:37 -0700

    fix

--- app/controllers/text_handler_controller.rb
+++ app/controllers/text_handler_controller.rb
@@ -8,23 +8,7 @@ class TextHandlerController < ApplicationController
     incoming_number = params["From"]
     
     if incoming_number == @number
-      if message_body[0..4].downcase == "update "
-        argument = message_body[5..-1].downcase
-        completed_activity = Activity.find_by_name(argument)
-        if !completed_activity.nil?
-          id = completed_activity.id
-          redirect_to :controller => 'home', :action => 'post_update', :id => id
-          send_text_message("The task #{completed_activity.name} was successfully updated.")
-        else
-          send_text_message("Error - #{argument} was not recognized as a task. No tasks were updated.")
-        end
-      elsif message_body[0..3].downcase = "list"
-        send_list()
-      elsif message_body[0..5].downcase == "status"
-        send_status()
-      else
-        send_text_message("Invalid command. For a list of commands, text me the word help")
-      end
+      send_text_message("Hi")
     end
   end
   
