7d3b9dabb1aa0854e3fbcdfabb6e16dfd0ec2f68
Rahul Mitra <rahulmitra.az@gmail.com>
2012-08-20 18:10:01 -0700

    fixed render issue

--- app/controllers/text_handler_controller.rb
+++ app/controllers/text_handler_controller.rb
@@ -38,6 +38,7 @@ class TextHandlerController < ApplicationController
         send_text_message("Invalid command. For a list of commands, text the word \'commands\'")
       end
     end
+    render :nothing => true
   end
-  render :nothing => true
+
 end
