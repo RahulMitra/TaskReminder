6b55a196f31a4983e0869cb037680a12458942cf
Rahul Mitra <rahulmitra.az@gmail.com>
2012-08-30 17:40:01 -0700

    fixed db type

--- db/migrate/20120827201457_create_activities.rb
+++ db/migrate/20120827201457_create_activities.rb
@@ -2,7 +2,7 @@ class CreateActivities < ActiveRecord::Migration
   def change
     create_table :activities do |t|
       t.string :name
-      t.string :user_id
+      t.integer :user_id
       t.integer :time_out
       t.date :time
       t.boolean :text_message
